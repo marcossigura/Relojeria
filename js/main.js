0de59f468f7aa47e7a2a76725d477cf4cd9f13e7
Marcos Sigura <marcossigura@gmail.com>
2026-08-31 13:40:27 -0300

    agregando bucles y condicionales

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-let nombre=prompt("Ingrese su nombre:");
-let apellido=prompt("Ingrese su apellido:");
-let anio= parseInt(prompt("Ingrese su año de nacimiento:"));
-let anioActual=parseInt(prompt("Ingrese el año actual:"));
+const veces=parseInt(prompt("Ingrese la cantidad de personas a calcular su edad:"));
 
-let edad=anioActual-anio;
+for(let i=0;i<veces;i++){
 
-alert("Bienvenido " + nombre+" "+apellido + ", su edad es: " + edad + " años");
-console.log("Bienvenido " + nombre+" "+apellido + ", su edad es: " + edad + " años");
+    let nombre=prompt("Ingrese su nombre:");
+    let apellido=prompt("Ingrese su apellido:");
+    let anio= parseInt(prompt("Ingrese su año de nacimiento:"));
+    let anioActual=parseInt(prompt("Ingrese el año actual:"));
+
+    const edad=anioActual-anio;
+
+    if(edad>=18){
+       alert("Bienvenido " + nombre+" "+apellido + ", su edad es: " + edad + " años y eres mayor de edad");
+       console.log("Bienvenido " + nombre+" "+apellido + ", su edad es: " + edad + " años y eres mayor de edad");
+    }else{
+    alert("Bienvenido " + nombre+" "+apellido + ", su edad es: " + edad + " años y eres menor de edad");
+       console.log("Bienvenido " + nombre+" "+apellido + ", su edad es: " + edad + " años y eres menor de edad");
+    }
+}
+
+
+
